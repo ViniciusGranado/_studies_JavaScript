b9107984b4da153485e1932993a25597032018f1
Vinicius Granado <vinicius.r.granado@gmail.com>
2020-04-11 15:29:04 -0300

    Added solving of challenge 002

---
+++ _javascript_challenges_curso_em_video/challenge002/resolucao/script.js
@@ -0,0 +1,5 @@
+function showGreetingsMessage(){
+    let userName = prompt(`Qual é o seu nome?`)
+    let userAge = prompt(`Olá, ${userName}! Quantos anos você tem?`)
+    alert(`Acabei de conhecer ${userName}, que tem ${userAge} anos de idade!`)
+}
