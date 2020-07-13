ecfd6e0c67cd46602a746aad711c3ab9581e797d
Vinicius Granado <vinicius.r.granado@gmail.com>
2020-07-13 18:12:13 -0300

    Added solving of exercise042

---
+++ _nodeJs_studies/_exercises/exercise042/exercise042.js
@@ -0,0 +1,135 @@
+// Module import
+const prompt = require('prompt-sync')();
+
+// Functions
+function getTriangleSides () {
+  const messages = ['Primeiro segmento: ', 'Segundo segmento: ', 'Terceiro segmento: '];
+  const triangleSides = [];
+
+  for (let i = 0; i < 3; i++) {
+
+    while (true) {
+      const triangleSideStr = prompt(messages[i]);
+
+      if (validateValue(triangleSideStr)) {
+        triangleSides.push(Number(triangleSideStr.trim().replace(',', '.')));
+        console.log('');
+        break;
+      } else {
+        console.log('Valor inválido, tente novamente.\n');
+      };
+    };
+  }
+
+  return triangleSides;
+}
+
+function validateValue (value) {
+
+  if (!value) {
+    return false
+  }
+
+  const convertedValue = Number(value.trim().replace(',', '.'));
+  if (isNaN(convertedValue) || convertedValue < 0) {
+    return false;
+  }
+
+  return true;
+}
+
+function checkIfValuesFormTriangle (valuesArray) {
+  const side0 = valuesArray[0];
+  const side1 = valuesArray[1];
+  const side2 = valuesArray[2];
+
+  return (side0 + side1 > side2) && (side1 + side2 > side0) && (side0 + side2 > side1);
+}
+
+// Main code
+console.log('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+console.log('Analisador de Triângulos')
+console.log('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+
+const triangleSides = getTriangleSides();
+const valuesFormTriangle = checkIfValuesFormTriangle(triangleSides);
+
+if (valuesFormTriangle) {
+  console.log('Os segmentos acima PODEM formar um triângulo!\n');
+} else {
+  console.log('Os segmentos acima NÃO PODEM formar um triângulo!\n');
+};
+
+prompt('Aperte Enter para finalizar.')// Module import
+const prompt = require('prompt-sync')();
+
+// Functions
+function getTriangleSides () {
+  const messages = ['Primeiro segmento: ', 'Segundo segmento: ', 'Terceiro segmento: '];
+  const triangleSides = [];
+
+  for (let i = 0; i < 3; i++) {
+    while (true) {
+      const triangleSideStr = prompt(messages[i]);
+
+      if (validateValue(triangleSideStr)) {
+        triangleSides.push(+triangleSideStr.trim().replace(',', '.'));
+        console.log('');
+        break;
+      } else {
+        console.log('Valor inválido, tente novamente.\n');
+      };
+    };
+  }
+
+  return triangleSides;
+}
+
+function validateValue (value) {
+  if (!value) {
+    return false
+  }
+
+  if (isNaN(value) || +value < 0) {
+    return false;
+  }
+
+  return true;
+}
+
+function checkIfValuesFormTriangle (valuesArray) {
+  const side0 = valuesArray[0];
+  const side1 = valuesArray[1];
+  const side2 = valuesArray[2];
+
+  return (side0 + side1 > side2) && (side1 + side2 > side0) && (side0 + side2 > side1);
+}
+
+function getTriangleType (sidesArray) {
+  if (sidesArray [0] === sidesArray[1] && sidesArray[1] === sidesArray[2]) {
+    return 'EQUILÁTERO';
+  } else if (sidesArray[0] !== sidesArray[1] && sidesArray[1] !== sidesArray[2]) {
+    return 'ESCALENO';
+  } else {
+    return 'ISÓSCELES';
+  }
+}
+
+// Main code
+console.log('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+console.log('Analisador de Triângulos')
+console.log('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+
+const triangleSides = getTriangleSides();
+const valuesFormTriangle = checkIfValuesFormTriangle(triangleSides);
+
+if (valuesFormTriangle) {
+  const triangleType = getTriangleType(triangleSides);
+
+  console.log(`Os segmentos acima PODEM formar um triângulo ${triangleType}!\n`);
+} else {
+  console.log('Os segmentos acima NÃO PODEM formar um triângulo!\n');
+};
+
+prompt('Aperte Enter para finalizar.')
+
