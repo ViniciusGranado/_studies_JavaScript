94d798d3443754fd7c0da93bca82cf2196549734
Vinicius Granado <vinicius.r.granado@gmail.com>
2020-07-26 13:40:40 -0300

    Added solving of exercise097.js

---
+++ _nodeJs_studies/_exercises/exercise097/exercise097.js
@@ -0,0 +1,11 @@
+// Functions
+function write (sentence) {
+  console.log('-'.repeat(sentence.length + 4));
+  console.log('  ' + sentence + '  ');
+  console.log('-'.repeat(sentence.length + 4));
+};
+
+// Main code
+write('Vinícius Granado');
+write('Curso de python no YouTube');
+write('CeV');
