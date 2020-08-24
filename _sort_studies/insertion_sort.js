5307080dee70cadea0223f95d91439bed18998fe
Vinicius Granado <vinicius.r.granado@gmail.com>
2020-08-24 17:26:52 -0300

    Added Insertion Sort

---
+++ _sort_studies/insertion_sort.js
@@ -0,0 +1,14 @@
+function insertionSort (list) {
+  for (let i = 1; i < list.length; i++) {
+    const key = list[i];
+
+    let j = i - 1;
+    while (j >= 0 && list[j] > key) {
+      list[j + 1] = list[j];
+      j--;
+    };
+
+    list[j + 1] = key;
+  }
+  return list;
+}
