a566a9a8f72dd2ba6f76d0071aad8a4a1984cf5a
Vinicius Granado <vinicius.r.granado@gmail.com>
2020-09-11 18:25:52 -0300

    Update basic_sql_node_test.js

--- node_sql_applications_studies/basic_sql_node_test.js
+++ node_sql_applications_studies/basic_sql_node_test.js
@@ -14,4 +14,37 @@ const Post = sequelize.define('post', {
   }
 });
 
-Post.sync({force: true});
+
+// User
+const User = sequelize.define('users', {
+  name: {
+    type: Sequelize.STRING  
+  },
+  surname: {
+    type: Sequelize.STRING
+  },
+  age: {
+    type: Sequelize.INTEGER
+  },
+  email: {
+    type: Sequelize.STRING
+  }
+})
+
+// Post.sync({force: true});
+// User.sync({force: true});
+
+Post.create({
+  title: 'Lorem Ipsum Dolor',
+  content: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Cum tenetur totam harum quo, non nisi laborum ipsam animi!
+            Quisquam facilis reiciendis laborum quas aliquam necessitatibus
+            neque reprehenderit possimus adipisci totam!`
+});
+
+User.create({
+  name: 'Vinícius',
+  surname: 'Granado',
+  age: 25,
+  email: 'vinicius.r.granado@gmail.com'
+});
