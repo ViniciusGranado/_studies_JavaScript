30f7edb39faa09abb583d724abd374b8ab9061de
Vinicius Granado <vinicius.r.granado@gmail.com>
2026-07-25 20:11:01 -0300

    Add server routing

--- node/node-server-example/index.js
+++ node/node-server-example/index.js
@@ -4,13 +4,60 @@ const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = createServer((req, res) => {
-    console.log(`req: ${req}`);
-    console.log(req);
-    console.log(`res: ${res}`);
+    req.on("error", (err) => {
+        console.log(err);
 
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.end('Hello World');
+        res.statusCode = 400;
+        res.end();
+
+        return;
+    })
+
+    res.on("error", (err) => {
+        console.log(err);
+    })
+
+    if (req.method === "GET") {
+        if (req.url === "/") {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "text/html");
+            res.end('Hello World');
+
+            return;
+        }
+
+        if (req.url === "/users") {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.end(JSON.stringify([
+                {id: 1, name: "John Doe"},
+                {id: 2, name: "Mary Jane"},
+            ]))
+
+            return;
+        }
+    }
+
+    if (req.method === "POST") {
+        if (req.url === "/users") {
+            let body = [];
+
+            req.on("data", (chunk) => {
+                body.push(chunk);
+            })
+            .on("end", () => {
+                body = Buffer.concat(body).toString();
+                res.end(body);
+
+                console.log(body);
+            })
+
+            return;
+        }
+    }
+
+    res.statusCode = 404;
+    res.end();
 });
 
 server.listen(port, hostname, () => {
